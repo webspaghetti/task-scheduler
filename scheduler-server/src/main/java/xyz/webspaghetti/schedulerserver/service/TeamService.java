0e7f1ab85ef0e3ed90bef433bb8771644951bff2
webspaghetti <ondrakohondra@email.cz>
2026-03-25 21:31:58 +0100

    Add Service method to find team by id

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/TeamService.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/TeamService.java
@@ -2,8 +2,10 @@ package xyz.webspaghetti.schedulerserver.service;
 
 import org.springframework.stereotype.Service;
 import xyz.webspaghetti.schedulerserver.dto.DtoStaticHelper;
+import xyz.webspaghetti.schedulerserver.dto.TeamResponseDto;
 import xyz.webspaghetti.schedulerserver.dto.UserResponseDto;
 import xyz.webspaghetti.schedulerserver.entity.Team;
+import xyz.webspaghetti.schedulerserver.mapper.TeamMapper;
 import xyz.webspaghetti.schedulerserver.mapper.UserMapper;
 import xyz.webspaghetti.schedulerserver.repository.TeamRepository;
 
@@ -14,12 +16,15 @@ public class TeamService {
 
     private final TeamRepository teamRepository;
     private final UserMapper userMapper;
+    private final TeamMapper teamMapper;
 
 
-    public TeamService(TeamRepository teamRepository, UserMapper userMapper) {
+    public TeamService(TeamRepository teamRepository, UserMapper userMapper, TeamMapper teamMapper) {
         this.teamRepository = teamRepository;
         this.userMapper = userMapper;
+        this.teamMapper = teamMapper;
     }
+
 
     public List<UserResponseDto> findAllTeamUsers(long teamId) {
 
@@ -29,7 +34,15 @@ public class TeamService {
                                 "Could not find team with id: " + teamId
                         ));
 
-        return DtoStaticHelper.userListToDtoList(tempTeam.getUsers(), userMapper);
         return DtoStaticHelper.userCollectionToDtoList(tempTeam.getUsers(), userMapper);
+    }
+
+    public TeamResponseDto findTeamById(long teamId) {
+
+        return teamMapper.toResponseDto(
+                teamRepository.findById(teamId).orElseThrow(() ->
+                        new RuntimeException(
+                                "Could not find team with id: " + teamId
+                        )));
     }
 }
