b44a315cbeeb53fcc841e242b8b1e04838db4236
webspaghetti <ondrakohondra@email.cz>
2026-03-25 21:33:09 +0100

    Add function to find tasks for user in a team

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/TaskRepository.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/TaskRepository.java
@@ -1,7 +1,14 @@
 package xyz.webspaghetti.schedulerserver.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import xyz.webspaghetti.schedulerserver.entity.Task;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Query("SELECT t FROM Task t JOIN t.users u WHERE u.id = :user_id AND t.team.id = :team_id")
+    List<Task> findTasksInTeamByUser(@Param("user_id") long userId, @Param("team_id") long teamId);
 }
--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/TaskService.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/TaskService.java
@@ -1,17 +1,28 @@
 package xyz.webspaghetti.schedulerserver.service;
 
 import org.springframework.stereotype.Service;
+import xyz.webspaghetti.schedulerserver.dto.DtoStaticHelper;
+import xyz.webspaghetti.schedulerserver.dto.TaskResponseDto;
+import xyz.webspaghetti.schedulerserver.mapper.TaskMapper;
 import xyz.webspaghetti.schedulerserver.repository.TaskRepository;
 
+import java.util.List;
 
 @Service
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
 
 
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
     }
 
+
+    public List<TaskResponseDto> findTasksForUserInTeam(long userId, long teamId) {
+
+        return DtoStaticHelper.taskCollectionToDtoList(taskRepository.findTasksInTeamByUser(userId, teamId), taskMapper);
+    }
 }
