57e62b0006238f83bf3d967840c47fb4450e23ff
webspaghetti <ondrakohondra@email.cz>
2026-03-25 13:38:44 +0100

    Add Team entity

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/entity/Team.java
@@ -0,0 +1,63 @@
+package xyz.webspaghetti.schedulerserver.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "teams")
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "team",
+               cascade = {CascadeType.DETACH, CascadeType.PERSIST,
+                          CascadeType.MERGE, CascadeType.REFRESH})
+    private List<Task> tasks;
+
+
+    public Team() {}
+    public Team(String name) {
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
