94dce874f1c2df30aef3e8bb165512261ce4ee94
webspaghetti <ondrakohondra@email.cz>
2026-03-25 18:53:57 +0100

    Add userListToDtoList static helper method

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/dto/DtoStaticHelper.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/dto/DtoStaticHelper.java
@@ -1,7 +1,25 @@
 package xyz.webspaghetti.schedulerserver.dto;
 
+import xyz.webspaghetti.schedulerserver.entity.User;
+import xyz.webspaghetti.schedulerserver.mapper.UserMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public final class DtoStaticHelper {
 
     private DtoStaticHelper(){}
 
+    // Helper method to cast plain User into UserResponseDto
+    public static List<UserResponseDto> userListToDtoList(List<User> userList, UserMapper userMapper) {
+
+        List<UserResponseDto> userResponseDtoList = new ArrayList<>();
+
+        for (User tempUser : userList) {
+
+            userResponseDtoList.add(userMapper.toResponseDto(tempUser));
+        }
+
+        return userResponseDtoList;
+    }
 }
