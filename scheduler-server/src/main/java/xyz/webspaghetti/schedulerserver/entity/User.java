d768b96246daa0ef06269a47c04f8525184e0f3d
webspaghetti <ondrakohondra@email.cz>
2026-03-25 13:38:09 +0100

    Add User entity

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/entity/User.java
@@ -0,0 +1,102 @@
+package xyz.webspaghetti.schedulerserver.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name", length = 30)
+    private String firstName;
+
+    @Column(name = "last_name", length = 30)
+    private String lastName;
+
+    @Column(name = "username", length = 50, unique = true, nullable = false)
+    private String username;
+
+    @JsonIgnore
+    @Column(name = "password", length = 80, nullable = false)
+    private String password;
+
+    @JsonIgnore
+    @Column(name = "enabled")
+    private boolean enabled;
+
+
+    public User() {}
+    public User(String firstName, String lastName, String username, String password, boolean enabled) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", enabled=" + enabled +
+                '}';
+    }
+}
