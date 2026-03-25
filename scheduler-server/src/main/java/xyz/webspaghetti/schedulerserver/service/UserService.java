0794e4cfcd123b71ef21cb06dd5ccd39828a0427
webspaghetti <ondrakohondra@email.cz>
2026-03-25 18:57:53 +0100

    Remove helper method from the User service

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/UserService.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/UserService.java
@@ -1,13 +1,8 @@
 package xyz.webspaghetti.schedulerserver.service;
 
 import org.springframework.stereotype.Service;
-import xyz.webspaghetti.schedulerserver.dto.UserResponseDto;
-import xyz.webspaghetti.schedulerserver.entity.User;
 import xyz.webspaghetti.schedulerserver.mapper.UserMapper;
 import xyz.webspaghetti.schedulerserver.repository.UserRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class UserService {
@@ -19,19 +14,6 @@ public class UserService {
     public UserService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
-    }
-
-    // Helper method to cast plain User into UserResponseDto
-    public List<UserResponseDto> entityListToDtoList(List<User> userList) {
-
-        List<UserResponseDto> userResponseDtoList = new ArrayList<>();
-
-        for (User tempUser : userList) {
-
-            userResponseDtoList.add(userMapper.toResponseDto(tempUser));
-        }
-
-        return userResponseDtoList;
     }
 
 }
