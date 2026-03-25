37edc8bf374d245a1a1a394270d2e3cd2f0c5d8a
webspaghetti <ondrakohondra@email.cz>
2026-03-25 11:28:33 +0100

    Initial commit

---
+++ scheduler-server/src/test/java/xyz/webspaghetti/schedulerserver/SchedulerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.webspaghetti.schedulerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
