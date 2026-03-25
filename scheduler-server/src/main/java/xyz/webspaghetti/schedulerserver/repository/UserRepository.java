9fd4947a3252ed6bd4941ae4965e62b74b1c8b5a
webspaghetti <ondrakohondra@email.cz>
2026-03-25 18:17:54 +0100

    Fix incorrect ID type

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/UserRepository.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/UserRepository.java
@@ -3,5 +3,5 @@ package xyz.webspaghetti.schedulerserver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import xyz.webspaghetti.schedulerserver.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
