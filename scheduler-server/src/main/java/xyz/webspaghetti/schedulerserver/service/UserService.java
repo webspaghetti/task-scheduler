373056095ffe3bbc982d7fb6657d4ab983389049
webspaghetti <ondrakohondra@email.cz>
2026-03-25 18:00:41 +0100

    Implement helper method for casting into DTO from entity

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/UserService.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/UserService.java
@@ -1,8 +1,13 @@
 package xyz.webspaghetti.schedulerserver.service;
 
 import org.springframework.stereotype.Service;
+import xyz.webspaghetti.schedulerserver.dto.UserResponseDto;
+import xyz.webspaghetti.schedulerserver.entity.User;
 import xyz.webspaghetti.schedulerserver.mapper.UserMapper;
 import xyz.webspaghetti.schedulerserver.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -14,6 +19,19 @@ public class UserService {
     public UserService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+    }
+
+    // Helper method to cast plain User into UserResponseDto
+    public List<UserResponseDto> entityListToDtoList(List<User> userList) {
+
+        List<UserResponseDto> userResponseDtoList = new ArrayList<>();
+
+        for (User tempUser : userList) {
+
+            userResponseDtoList.add(userMapper.toResponseDto(tempUser));
+        }
+
+        return userResponseDtoList;
     }
 
 }
