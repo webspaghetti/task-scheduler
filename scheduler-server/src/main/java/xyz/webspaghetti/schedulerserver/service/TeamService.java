1d6dac6e52b78f5d032922fc613a28694bfa878e
webspaghetti <ondrakohondra@email.cz>
2026-03-25 19:00:41 +0100

    Add method for finding all users per team

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/TeamService.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/TeamService.java
@@ -1,16 +1,34 @@
 package xyz.webspaghetti.schedulerserver.service;
 
 import org.springframework.stereotype.Service;
+import xyz.webspaghetti.schedulerserver.dto.DtoStaticHelper;
+import xyz.webspaghetti.schedulerserver.dto.UserResponseDto;
+import xyz.webspaghetti.schedulerserver.entity.Team;
+import xyz.webspaghetti.schedulerserver.mapper.UserMapper;
 import xyz.webspaghetti.schedulerserver.repository.TeamRepository;
+
+import java.util.List;
 
 @Service
 public class TeamService {
 
     private final TeamRepository teamRepository;
+    private final UserMapper userMapper;
 
 
-    public TeamService(TeamRepository teamRepository) {
+    public TeamService(TeamRepository teamRepository, UserMapper userMapper) {
         this.teamRepository = teamRepository;
+        this.userMapper = userMapper;
     }
 
+    public List<UserResponseDto> findAllTeamUsers(long teamId) {
+
+        Team tempTeam =
+                teamRepository.findById(teamId).orElseThrow(() ->
+                        new RuntimeException(
+                                "Could not find team with id: " + teamId
+                        ));
+
+        return DtoStaticHelper.userListToDtoList(tempTeam.getUsers(), userMapper);
+    }
 }
