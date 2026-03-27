1c889b773b112ae35b3c4943c662479edfcc4050
webspaghetti <ondrakohondra@email.cz>
2026-03-27 11:57:15 +0100

    Add toEntity for Task mapper

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/mapper/TaskMapper.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/mapper/TaskMapper.java
@@ -2,11 +2,14 @@ package xyz.webspaghetti.schedulerserver.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import xyz.webspaghetti.schedulerserver.dto.TaskCreateDto;
 import xyz.webspaghetti.schedulerserver.dto.TaskResponseDto;
 import xyz.webspaghetti.schedulerserver.entity.Task;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
+
+    Task toEntity(TaskCreateDto taskCreateDto);
 
     @Mapping(target = "teamId", source = "team.id")
     TaskResponseDto toResponseDto(Task task);
