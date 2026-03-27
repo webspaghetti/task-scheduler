ece2ae297020256119c7fd9b0952cd8d573094e3
webspaghetti <ondrakohondra@email.cz>
2026-03-27 12:25:48 +0100

    Add toEntity for Team Mapper

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/mapper/TeamMapper.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/mapper/TeamMapper.java
@@ -1,11 +1,14 @@
 package xyz.webspaghetti.schedulerserver.mapper;
 
 import org.mapstruct.Mapper;
+import xyz.webspaghetti.schedulerserver.dto.TeamCreateDto;
 import xyz.webspaghetti.schedulerserver.dto.TeamResponseDto;
 import xyz.webspaghetti.schedulerserver.entity.Team;
 
 @Mapper(componentModel = "spring")
 public interface TeamMapper {
+
+    Team toEntity(TeamCreateDto teamCreateDto);
 
     TeamResponseDto toResponseDto(Team team);
 }
