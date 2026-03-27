86872be48587794c2fbcd3f5fcc5ba65c0726bc2
webspaghetti <ondrakohondra@email.cz>
2026-03-27 11:38:14 +0100

    Add function for creating Users

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/UserService.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/UserService.java
@@ -1,20 +1,29 @@
 package xyz.webspaghetti.schedulerserver.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import xyz.webspaghetti.schedulerserver.dto.UserCreateDto;
 import xyz.webspaghetti.schedulerserver.dto.UserResponseDto;
+import xyz.webspaghetti.schedulerserver.entity.Role;
+import xyz.webspaghetti.schedulerserver.entity.User;
 import xyz.webspaghetti.schedulerserver.mapper.UserMapper;
+import xyz.webspaghetti.schedulerserver.repository.RoleRepository;
 import xyz.webspaghetti.schedulerserver.repository.UserRepository;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
@@ -25,5 +34,26 @@ public class UserService {
                         new RuntimeException(
                                 "Could not find user with id: " + userId
                         )));
+    }
+
+    public UserResponseDto createUser(UserCreateDto userCreateDto) {
+
+        // Create temp User object and encode its password
+        User tempUser = userMapper.toEntity(userCreateDto);
+        tempUser.setPassword(passwordEncoder.encode(userCreateDto.password()));
+
+        int defaultRoleId = 1;
+
+        // Give User default ROLE_USER
+        Role tempRole = roleRepository.findById(defaultRoleId).orElseThrow(() ->
+                new RuntimeException(
+                        "Could not find role with id: " + defaultRoleId
+                ));
+        tempUser.addRole(tempRole);
+
+        // Create User object returned from persisting
+        User savedUser = userRepository.save(tempUser);
+
+        return userMapper.toResponseDto(savedUser);
     }
 }
