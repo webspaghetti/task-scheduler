e9c814b8b1fa0beade23bc88bc395bfcbbc039a2
webspaghetti <ondrakohondra@email.cz>
2026-03-28 19:25:03 +0100

    Add custom exceptions into handler

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/exception/GlobalExceptionHandler.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/exception/GlobalExceptionHandler.java
@@ -1,7 +1,71 @@
 package xyz.webspaghetti.schedulerserver.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import xyz.webspaghetti.schedulerserver.dto.response.ErrorResponseDto;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleEntityNotFound(EntityNotFoundException e) {
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                HttpStatus.NOT_FOUND.value(),
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserNotInTeamException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserNotInTeam(UserNotInTeamException e) {
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                HttpStatus.BAD_REQUEST.value(),
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserAlreadyInTeamException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserAlreadyInTeam(UserAlreadyInTeamException e) {
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                HttpStatus.CONFLICT.value(),
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UserNotAssignedToTaskException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserNotAssignedToTask(UserNotAssignedToTaskException e) {
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                HttpStatus.BAD_REQUEST.value(),
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserAlreadyAssignedToTaskException.class)
+    public ResponseEntity<ErrorResponseDto> handleUserAlreadyAssignedToTask(UserAlreadyAssignedToTaskException e) {
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                HttpStatus.CONFLICT.value(),
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.CONFLICT);
+    }
 }
