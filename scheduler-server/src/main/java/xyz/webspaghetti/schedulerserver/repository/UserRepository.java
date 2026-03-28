74bbe26a6935002f6795284468afa817336c0937
webspaghetti <ondrakohondra@email.cz>
2026-03-28 18:37:27 +0100

    Remove redundant method

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/UserRepository.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/UserRepository.java
@@ -2,11 +2,5 @@ package xyz.webspaghetti.schedulerserver.repository;
 
 import xyz.webspaghetti.schedulerserver.entity.User;
 
-import java.util.Optional;
-
-
 public interface UserRepository extends BaseRepository<User, Integer> {
-
-
-    Optional<User> findById(Integer id);
 }
