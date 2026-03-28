df4d345d717e1b2180c651ad60b63a5188ae765d
webspaghetti <ondrakohondra@email.cz>
2026-03-28 19:24:11 +0100

    Add global Exception handler

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package xyz.webspaghetti.schedulerserver.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
