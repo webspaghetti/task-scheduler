25318fe2cfe21829657ee037c0729fec95580050
webspaghetti <ondrakohondra@email.cz>
2026-03-29 15:23:09 +0200

    Fix path patterns

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/SecurityConfig.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/SecurityConfig.java
@@ -53,27 +53,27 @@ public class SecurityConfig {
                         // Protected endpoints
                         .requestMatchers(HttpMethod.GET, "/api/users/**").hasRole("USER")
                         .requestMatchers(HttpMethod.GET, "/api/teams/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.GET, "/api/teams/**/users").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/api/teams/*/users").hasRole("USER")
                         .requestMatchers(HttpMethod.GET, "/api/tasks/**").hasRole("USER")
                         .requestMatchers(HttpMethod.GET, "/api/tasks/teams/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.GET, "/api/tasks/teams/**/users/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/api/tasks/teams/*/users/**").hasRole("USER")
 
-                        .requestMatchers(HttpMethod.POST, "/api/users/**/roles/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/users/*/roles/**").hasAnyRole("MANAGER", "ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/teams").hasAnyRole("MANAGER", "ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/teams/**/users/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/teams/*/users/**").hasAnyRole("MANAGER", "ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/tasks").hasAnyRole("MANAGER", "ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/tasks/**/users/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/tasks/*/users/**").hasAnyRole("MANAGER", "ADMIN")
 
                         .requestMatchers(HttpMethod.PUT, "/api/users/**").hasRole("USER")
                         .requestMatchers(HttpMethod.PUT, "/api/teams/**").hasAnyRole("MANAGER", "ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/users/**").hasAnyRole("MANAGER", "ADMIN")
 
                         .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/api/users/**/roles/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/*/roles/**").hasAnyRole("MANAGER", "ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/teams/**").hasAnyRole("MANAGER", "ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/api/teams/**/users/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/teams/*/users/**").hasAnyRole("MANAGER", "ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/tasks/**").hasAnyRole("MANAGER", "ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/api/tasks/**/users/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/tasks/*/users/**").hasAnyRole("MANAGER", "ADMIN")
 
                         // Everything else
                         .anyRequest().authenticated()
