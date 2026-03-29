ec84522fd90591994bf0445b376b8500b2931706
webspaghetti <ondrakohondra@email.cz>
2026-03-29 15:20:14 +0200

    Update security config with new API endpoints

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/SecurityConfig.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/SecurityConfig.java
@@ -47,13 +47,36 @@ public class SecurityConfig {
 
         httpSecurity.authorizeHttpRequests(configurer ->
                 configurer
-                        .requestMatchers(HttpMethod.GET, "/api/test/**").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.GET, "/api/swagger-config").hasRole("USER")
-                        .requestMatchers(HttpMethod.POST, "/api/post-test").hasRole("USER")
-                        .requestMatchers(HttpMethod.PUT, "/api/put-test/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/delete-test/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.POST, "/api/add-test/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/remove-test/**").hasRole("USER")
+                        // Public endpoints
+                        .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+
+                        // Protected endpoints
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/api/teams/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/api/teams/**/users").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/api/tasks/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/api/tasks/teams/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/api/tasks/teams/**/users/**").hasRole("USER")
+
+                        .requestMatchers(HttpMethod.POST, "/api/users/**/roles/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/teams").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/teams/**/users/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/tasks").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/tasks/**/users/**").hasAnyRole("MANAGER", "ADMIN")
+
+                        .requestMatchers(HttpMethod.PUT, "/api/users/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.PUT, "/api/teams/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/users/**").hasAnyRole("MANAGER", "ADMIN")
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/**/roles/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/teams/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/teams/**/users/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/tasks/**").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/tasks/**/users/**").hasAnyRole("MANAGER", "ADMIN")
+
+                        // Everything else
+                        .anyRequest().authenticated()
         );
 
         httpSecurity.httpBasic(Customizer.withDefaults());
