cf38b056c346b5b2c4a4e33a2755c4dae1c4e0e5
webspaghetti <ondrakohondra@email.cz>
2026-03-29 13:36:34 +0200

    Add roles List to User response DTO

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/dto/response/UserResponseDto.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/dto/response/UserResponseDto.java
@@ -1,10 +1,13 @@
 package xyz.webspaghetti.schedulerserver.dto.response;
 
+import java.util.List;
+
 // What we will be sending to users
 public record UserResponseDto(
         Integer id,
         String firstName,
         String lastName,
-        String username
+        String username,
+        List<RoleResponseDto> roles
 ) {
 }
