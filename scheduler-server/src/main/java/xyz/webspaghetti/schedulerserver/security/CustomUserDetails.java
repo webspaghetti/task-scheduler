9c836a018afb89bae9ef4bbea588642ae824d8c7
webspaghetti <ondrakohondra@email.cz>
2026-03-30 14:08:49 +0200

    Add custom implementation for UserDetails

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/CustomUserDetails.java
@@ -0,0 +1,62 @@
+package xyz.webspaghetti.schedulerserver.security;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+    private final Integer id;
+    private final String username;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+
+    public CustomUserDetails(Integer id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    // Standard UserDetails methods
+    @Override
+    public @NonNull Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @NonNull String getPassword() {
+        return password;
+    }
+
+    @Override
+    public @NonNull String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
