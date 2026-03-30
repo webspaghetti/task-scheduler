c9f0f932338d3b0bf45cf607c0e2d6c4b72c893a
webspaghetti <ondrakohondra@email.cz>
2026-03-30 14:23:37 +0200

    Add findByUsername for User Repository

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/UserRepository.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/UserRepository.java
@@ -2,5 +2,9 @@ package xyz.webspaghetti.schedulerserver.repository;
 
 import xyz.webspaghetti.schedulerserver.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends BaseRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
 }
