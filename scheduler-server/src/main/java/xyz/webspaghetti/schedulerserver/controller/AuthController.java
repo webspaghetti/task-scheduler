8f11a490d15a5f76f4e09a5a85abafa0db1bba53
webspaghetti <ondrakohondra@email.cz>
2026-03-30 14:18:58 +0200

    Add /login POST endpoint

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/controller/AuthController.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/controller/AuthController.java
@@ -1,8 +1,18 @@
 package xyz.webspaghetti.schedulerserver.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.webspaghetti.schedulerserver.dto.request.LoginRequestDto;
+import xyz.webspaghetti.schedulerserver.dto.response.JwtResponseDto;
+import xyz.webspaghetti.schedulerserver.security.CustomUserDetails;
 import xyz.webspaghetti.schedulerserver.security.JwtUtil;
 import xyz.webspaghetti.schedulerserver.service.UserService;
 
@@ -19,5 +29,33 @@ public class AuthController {
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
         this.userService = userService;
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(
+            @RequestBody LoginRequestDto loginRequestDto
+    ) {
+
+        try {
+            // Spring Security checks the username and password against the database
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequestDto.username(),
+                            loginRequestDto.password()
+                    )
+            );
+
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+            assert userDetails != null;
+            String jwt = jwtUtil.generateToken(userDetails);
+
+            return ResponseEntity.ok(new JwtResponseDto(jwt));
+        } catch (BadCredentialsException e) {
+
+            // If the password or username is wrong, they get a 401 Unauthorized
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Incorrect username or password");
+        }
     }
 }
