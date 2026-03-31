540535005a4db312c3c6c9add3062733ecab8126
webspaghetti <ondrakohondra@email.cz>
2026-03-31 19:41:39 +0200

    Add authorization method for checking if User is part of Team

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/authorization/TeamAuthorization.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/authorization/TeamAuthorization.java
@@ -1,7 +1,27 @@
 package xyz.webspaghetti.schedulerserver.security.authorization;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import xyz.webspaghetti.schedulerserver.security.CustomUserDetails;
+import xyz.webspaghetti.schedulerserver.service.TeamService;
 
 @Component("teamAuthorization")
 public class TeamAuthorization {
+
+    private final TeamService teamService;
+
+
+    public TeamAuthorization(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+
+    public boolean isMember(Integer teamId, Authentication authentication) {
+
+        if (!(authentication.getPrincipal() instanceof CustomUserDetails userDetails)) {
+            return false;
+        }
+
+        return teamService.isMemberOfTeam(userDetails.getId(), teamId);
+    }
 }
