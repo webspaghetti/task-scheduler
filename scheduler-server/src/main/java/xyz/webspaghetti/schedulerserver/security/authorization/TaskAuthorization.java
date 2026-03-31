a9e42b5b5de18ee5b67f94df029096115be21724
webspaghetti <ondrakohondra@email.cz>
2026-03-31 19:37:49 +0200

    Add Authorization class for Tasks

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/authorization/TaskAuthorization.java
@@ -0,0 +1,7 @@
+package xyz.webspaghetti.schedulerserver.security.authorization;
+
+import org.springframework.stereotype.Component;
+
+@Component("taskAuthorization")
+public class TaskAuthorization {
+}
