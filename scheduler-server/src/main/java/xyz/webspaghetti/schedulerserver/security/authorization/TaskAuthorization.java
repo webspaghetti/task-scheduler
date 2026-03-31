11da8806c908e6b056998a5f8a96be19f7f1e3d9
webspaghetti <ondrakohondra@email.cz>
2026-03-31 19:39:18 +0200

    Add authorization method for checking if User is part of Tasks team

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/authorization/TaskAuthorization.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/authorization/TaskAuthorization.java
@@ -1,7 +1,30 @@
 package xyz.webspaghetti.schedulerserver.security.authorization;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import xyz.webspaghetti.schedulerserver.security.CustomUserDetails;
+import xyz.webspaghetti.schedulerserver.service.TaskService;
+import xyz.webspaghetti.schedulerserver.service.TeamService;
 
 @Component("taskAuthorization")
 public class TaskAuthorization {
+
+    private final TaskService taskService;
+    private final TeamService teamService;
+
+
+    public TaskAuthorization(TaskService taskService, TeamService teamService) {
+        this.taskService = taskService;
+        this.teamService = teamService;
+    }
+
+
+    public boolean isInTaskTeam(Integer taskId, Authentication authentication) {
+
+        if (!(authentication.getPrincipal() instanceof CustomUserDetails userDetails)) {
+            return false;
+        }
+
+        return teamService.isMemberOfTeam(userDetails.getId(), taskService.findTaskById(taskId).teamId());
+    }
 }
