d0cae886dad43f86e32e161f4d136bf9dc651f59
webspaghetti <ondrakohondra@email.cz>
2026-03-31 19:40:55 +0200

    Add Authorization class for Teams

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/authorization/TeamAuthorization.java
@@ -0,0 +1,7 @@
+package xyz.webspaghetti.schedulerserver.security.authorization;
+
+import org.springframework.stereotype.Component;
+
+@Component("teamAuthorization")
+public class TeamAuthorization {
+}
