e713d62b40c22dc54ff5a271626a91fab21086fa
webspaghetti <ondrakohondra@email.cz>
2026-03-31 19:42:04 +0200

    Add authorization method for checking if User is manager and part of Team

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/authorization/TeamAuthorization.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/security/authorization/TeamAuthorization.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import xyz.webspaghetti.schedulerserver.security.CustomUserDetails;
 import xyz.webspaghetti.schedulerserver.service.TeamService;
 
+import java.util.Objects;
+
 @Component("teamAuthorization")
 public class TeamAuthorization {
 
@@ -23,5 +25,19 @@ public class TeamAuthorization {
         }
 
         return teamService.isMemberOfTeam(userDetails.getId(), teamId);
+    }
+
+    public boolean isManagerMember(Integer teamId, Authentication authentication) {
+
+        if (!(authentication.getPrincipal() instanceof CustomUserDetails userDetails)) {
+            return false;
+        }
+
+        boolean isTeamMember = teamService.isMemberOfTeam(userDetails.getId(), teamId);
+
+        boolean isManager = userDetails.getAuthorities().stream()
+                .anyMatch(a -> Objects.equals(a.getAuthority(), "ROLE_MANAGER"));
+
+        return (isTeamMember && isManager);
     }
 }
