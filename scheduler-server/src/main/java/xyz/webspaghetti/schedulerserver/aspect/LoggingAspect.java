b39ba18d3bab5c00591d8f023b11de1540c8de71
webspaghetti <ondrakohondra@email.cz>
2026-04-01 11:55:36 +0200

    Add aspect class for logging

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/aspect/LoggingAspect.java
@@ -0,0 +1,9 @@
+package xyz.webspaghetti.schedulerserver.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+}
