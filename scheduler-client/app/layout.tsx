e1ae7af73f73d361aa979449d205b71119f3a7e2
webspaghetti <ondrakohondra@email.cz>
2026-04-02 19:28:33 +0200

    Add suppressing of hydration warnings to root <body>

--- scheduler-client/app/layout.tsx
+++ scheduler-client/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           className={`${inter.variable} h-full antialiased`}
           suppressHydrationWarning
       >
-      <body className={`${inter.className} min-h-full flex flex-col`}>{children}</body>
+      <body className={`${inter.className} min-h-full flex flex-col`} suppressHydrationWarning>{children}</body>
       </html>
   );
 }
