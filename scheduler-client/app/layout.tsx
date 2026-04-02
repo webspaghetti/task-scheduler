c71176d2d207acff30a9bc0b5d4c7e809318e3e3
webspaghetti <ondrakohondra@email.cz>
2026-04-02 16:48:32 +0200

    Update page font to Inter

--- scheduler-client/app/layout.tsx
+++ scheduler-client/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -23,11 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+      <html
+          lang="en"
+          className={`${inter.variable} h-full antialiased`}
+      >
+      <body className={`${inter.className} min-h-full flex flex-col`}>{children}</body>
+      </html>
   );
 }
