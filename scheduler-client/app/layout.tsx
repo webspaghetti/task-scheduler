8203a4f49a3033927d03b37f5c052705ca3b80df
webspaghetti <ondrakohondra@email.cz>
2026-04-02 16:49:09 +0200

    Update root HTML tag to stop showing hydration warnings

--- scheduler-client/app/layout.tsx
+++ scheduler-client/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
       <html
           lang="en"
           className={`${inter.variable} h-full antialiased`}
+          suppressHydrationWarning
       >
       <body className={`${inter.className} min-h-full flex flex-col`}>{children}</body>
       </html>
