24cf804a731f652cc5b9a7630dcd8d1aecbac170
webspaghetti <ondrakohondra@email.cz>
2026-04-02 16:19:00 +0200

    Add exception for duplicate username

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package xyz.webspaghetti.schedulerserver.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
