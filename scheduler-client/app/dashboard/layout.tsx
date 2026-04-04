0f2a4399c8e6e71e5e27601ae3c1869014bb360d
webspaghetti <ondrakohondra@email.cz>
2026-04-04 14:24:44 +0200

    Update the root layout color

--- scheduler-client/app/dashboard/layout.tsx
+++ scheduler-client/app/dashboard/layout.tsx
@@ -112,7 +112,7 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex min-h-screen bg-[#E7E4F2]">
+        <div className="flex min-h-screen bg-[#EEECF6]">
             <Sidebar />
             <div className="flex-1 flex flex-col min-w-0">
                 {children}
