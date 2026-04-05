55abb1a11a9ce9a4cf6a818c477e016a22a5fba0
webspaghetti <ondrakohondra@email.cz>
2026-04-05 17:38:55 +0200

    Add Service for ActionHistory

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/ActionHistoryService.java
@@ -0,0 +1,9 @@
+package xyz.webspaghetti.schedulerserver.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActionHistoryService {
+}
