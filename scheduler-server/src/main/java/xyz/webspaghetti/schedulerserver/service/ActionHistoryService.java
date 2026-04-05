f9948499f3b8cb798239039c4cd8eec8933da692
webspaghetti <ondrakohondra@email.cz>
2026-04-05 17:39:24 +0200

    Add method for saving history into database

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/ActionHistoryService.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/service/ActionHistoryService.java
@@ -1,9 +1,26 @@
 package xyz.webspaghetti.schedulerserver.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import xyz.webspaghetti.schedulerserver.dto.create.ActionHistoryCreateDto;
+import xyz.webspaghetti.schedulerserver.dto.response.ActionHistoryResponseDto;
+import xyz.webspaghetti.schedulerserver.entity.ActionHistory;
+import xyz.webspaghetti.schedulerserver.mapper.ActionHistoryMapper;
+import xyz.webspaghetti.schedulerserver.repository.ActionHistoryRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ActionHistoryService {
+
+    private final ActionHistoryRepository actionHistoryRepository;
+    private final ActionHistoryMapper historyMapper;
+
+    @Transactional
+    public ActionHistoryResponseDto createHistory(ActionHistoryCreateDto actionHistoryCreateDto) {
+
+        ActionHistory savedHistory = historyMapper.toEntity(actionHistoryCreateDto);
+
+        return historyMapper.toResponseDto(actionHistoryRepository.save(savedHistory));
+    }
 }
