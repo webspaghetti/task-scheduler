5c870726c72452f42806bcaed894bb53fd9a81d2
webspaghetti <ondrakohondra@email.cz>
2026-04-05 17:37:01 +0200

    Remove redundant @Repository annotation

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/RoleRepository.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/RoleRepository.java
@@ -1,8 +1,6 @@
 package xyz.webspaghetti.schedulerserver.repository;
 
-import org.springframework.stereotype.Repository;
 import xyz.webspaghetti.schedulerserver.entity.Role;
 
-@Repository
 public interface RoleRepository extends BaseRepository<Role, Integer> {
 }
