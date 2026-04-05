e7fc7cf4e6fe64352c8a8df3bbc97f0bdf548b56
webspaghetti <ondrakohondra@email.cz>
2026-04-05 18:58:33 +0200

    Add userId to useAuth

--- scheduler-client/hooks/useAuth.ts
+++ scheduler-client/hooks/useAuth.ts
@@ -1,7 +1,8 @@
-import { hasRole, getCurrentUsername } from "@/lib/jwt";
+import { hasRole, getCurrentUsername, getCurrentUserId } from "@/lib/jwt";
 
 export function useAuth() {
     return {
+        userId: getCurrentUserId(),
         username: getCurrentUsername(),
         isAdmin: hasRole("ADMIN"),
         isManager: hasRole("MANAGER"),
