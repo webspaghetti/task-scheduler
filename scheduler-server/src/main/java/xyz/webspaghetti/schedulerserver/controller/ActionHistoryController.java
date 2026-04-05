61864d265245e3c8f75da109938d5dbdc2960939
webspaghetti <ondrakohondra@email.cz>
2026-04-05 21:25:17 +0200

    Add ActionHistory API endpoint to get history

--- scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/controller/ActionHistoryController.java
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/controller/ActionHistoryController.java
@@ -1,11 +1,29 @@
 package xyz.webspaghetti.schedulerserver.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.webspaghetti.schedulerserver.dto.response.ActionHistoryResponseDto;
+import xyz.webspaghetti.schedulerserver.service.ActionHistoryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/history")
 @RequiredArgsConstructor
 public class ActionHistoryController {
+
+    private final ActionHistoryService actionHistoryService;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<ActionHistoryResponseDto>> getHistory(){
+
+        List<ActionHistoryResponseDto> fetchedHistory = actionHistoryService.getHistory();
+
+        return ResponseEntity.ok(fetchedHistory);
+    }
 }
