e6c773205b839feba4faf20c24b8210d8fc1366e
webspaghetti <ondrakohondra@email.cz>
2026-04-05 21:17:39 +0200

    Add controller for ActionHistory

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/controller/ActionHistoryController.java
@@ -0,0 +1,11 @@
+package xyz.webspaghetti.schedulerserver.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/history")
+@RequiredArgsConstructor
+public class ActionHistoryController {
+}
