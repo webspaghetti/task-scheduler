344f2c26e417f41d9e4c68c0a6afa8b217cb343c
webspaghetti <ondrakohondra@email.cz>
2026-04-05 17:36:45 +0200

    Add Repository for ActionHistory

---
+++ scheduler-server/src/main/java/xyz/webspaghetti/schedulerserver/repository/ActionHistoryRepository.java
@@ -0,0 +1,6 @@
+package xyz.webspaghetti.schedulerserver.repository;
+
+import xyz.webspaghetti.schedulerserver.entity.ActionHistory;
+
+public interface ActionHistoryRepository extends BaseRepository<ActionHistory, Integer> {
+}
